5e7eaa3f6f936269dc3fe753684d5816c386985e
Pedro Kielblock <p.kielblock123@gmail.com>
2022-03-06 02:44:03 -0300

    First Version Finished

---
+++ delete.php
@@ -0,0 +1,17 @@
+<?php
+
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=products_crud', 'projects', '12345678');
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$id = $_POST['id'] ?? null;
+
+if (!$id) {
+    header('Location: index.php');
+    exit;
+}
+
+$statement = $pdo->prepare('DELETE FROM products WHERE id = :id');
+$statement->bindValue(':id', $id);
+$statement->execute();
+
+header('Location: index.php');
